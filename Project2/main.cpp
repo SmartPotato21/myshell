b693ecb7920fc1387e1c0034fe3ddfb4263d7efc
Vladislav Lyubimov <104612753+SmartPotato21@users.noreply.github.com>
2025-07-01 00:50:07 -0500

    task 3

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -3,6 +3,11 @@
 #include <thread>
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <chrono>
+#include <cstdlib>
+
+#define N 100000
 
 using namespace std;
 
@@ -14,7 +19,6 @@ void insertionSort(int* arr, int n)	//sorts all elements starting at int* up to 
 
     while (rightIndex < n)
     {
-		
         leftIndex = rightIndex-1;
 		
         while(arr[leftIndex] > arr[rightIndex] && leftIndex >= 0)
@@ -30,33 +34,91 @@ void insertionSort(int* arr, int n)	//sorts all elements starting at int* up to 
         i++;
 		rightIndex = i;
     }
+}
 
-	for	(int i = 0; i < n; i++)
+void merge(int* arr1,int* arr2, vector<int> &output, int half1, int half2)
+{
+	int n = half1*2;
+
+	for (int i = 0; i < n; i++)				// sort all even n elements
 	{
-		cout << arr[i] << " ";
+		if(arr1[0] < arr2[0] || half2 == 0)	//check if half2 has been fully checked or compare values
+		{
+			output.push_back(arr1[0]);
+			arr1++;							//point to next value
+		}
+		else 
+		{
+			output.push_back(arr2[0]);		
+			half2--;						
+			arr2++;							//point to next value
+		}
 	}
-	cout << "\n";
+
+	if (half2 != half1)						//sort remainder if odd n
+		output.push_back(arr2[0]);
+
+}
+
+void multithreadedSort(int* arr, int half1, int half2, int n)
+{
+	auto start = chrono::high_resolution_clock::now();
+
+
+	thread a(insertionSort,&arr[0],half1);
+	thread b(insertionSort,&arr[half1],half2);
+	a.join();
+    b.join();
+
+	vector<int> output;
+	output.reserve(n);
+
+	thread c(merge,&arr[0], &arr[half1], ref(output), half1, half2);
+	c.join();
+
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+	cout << "Multithreadded code took " << duration.count() << "ms \n";
+}
+void singlethreadedSort(int* arr, int half1, int half2, int n)
+{
+	auto start = chrono::high_resolution_clock::now();
+
+	insertionSort(&arr[0], half1);
+	insertionSort(&arr[half1], half2);
+
+	vector<int> output;
+	output.reserve(n);
+
+	merge(&arr[0], &arr[half1],output, half1,half2);
+
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+	cout << "Singlethreadded code took " << duration.count() << "ms \n";
 }
 
 
 int main()
 {
-    int arr[] = {5,6,7,5,9,8,9,8,6,7,2,1,55};          //array
-    int n = sizeof(arr) / sizeof(arr[0]);   // N
+    int arr1[N];          //array
+	int arr2[N];          //array
+
+	for (int i = 0; i < N; i++)
+	{
+		int x = rand() % 99999;
+		arr1[i] = x;
+		arr2[i] = x;
+	}
+    const int n = sizeof(arr1) / sizeof(arr1[0]);   // n
  	int half1 = n/2;
-	int half2 = (n+1)/2;			//incase there is odd N, round int up.
+	int half2 = (n+1)/2;			//incase there is odd n, round int up.
 
 
-    thread a(insertionSort,&arr[0],half1);
-    a.join();
-
-
-	thread b(insertionSort,&arr[6],half2);
-    b.join();
-
-
-
-
+	
+    multithreadedSort(arr1,half1,half2,n);
+	singlethreadedSort(arr2,half1,half2,n);
 
     return 0;
 }
